9619a8596f739f53a0d36e0da5b389a3794ec11b
fadlullan <fadlullahherifarid@gmail.com>
2024-07-26 02:24:44 +0800

    Revisi Tampilan

--- app/src/main/java/com/example/eskristal/SplashActivity.java
+++ app/src/main/java/com/example/eskristal/SplashActivity.java
@@ -4,21 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.eskristal.Adapter.SessionManager;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_DISPLAY_LENGTH = 3000; // 2 detik
+    private static final int SPLASH_DISPLAY_LENGTH = 3000; // 3 detik
+    private SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        sessionManager = new SessionManager(this);
+
         new Handler().postDelayed(() -> {
-            // Pindah ke aktivitas utama setelah waktu splash screen berakhir
-            Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(mainIntent);
-            finish(); // Tutup splash screen activity
+            if (sessionManager.isLoggedIn()) {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            } else {
+                Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+            finish();
         }, SPLASH_DISPLAY_LENGTH);
     }
 }
